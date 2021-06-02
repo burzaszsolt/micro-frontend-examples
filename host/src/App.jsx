8a9a753b69b66eb13e9a63f40e6324be4c8406b3
Zsolt Burzas <zsolt.burzas@infinitaslearning.com>
2021-06-02 07:11:04 +0200

    add routing to host

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,16 +1,45 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { App1 } from "./components/App1";
 import { App2 } from "./components/App2";
 import { App3 } from "./components/App3";
 
 const App = () => (
-  <div>
-    Host app
-    <App1 />
-    <App2 />
-    <App3 />
-  </div>
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/app1">App1</Link>
+          </li>
+          <li>
+            <Link to="/app2">App2</Link>
+          </li>
+          <li>
+            <Link to="/app3">App3</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/app1">
+          <App1 />
+        </Route>
+        <Route path="/app2">
+          <App2 />
+        </Route>
+        <Route path="/app3">
+          <App3 />
+        </Route>
+        <Route path="/" exact>
+          <p>Home</p>
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
